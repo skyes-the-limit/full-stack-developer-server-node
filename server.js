00ff31e0c77b2e610479e7ff21569ebfd3d42542
skyes-the-limit <riversong477@gmail.com>
2022-03-29 19:25:42 -0400

    equesting data from a RESTful Web server API from React

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 import express from 'express';
+import cors from 'cors';
+
 import helloController from "./controllers/hello-controller.js";
 import tuitsController from './controllers/tuits-controller.js';
 import userController from "./controllers/users-controller.js";
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 helloController(app);
 userController(app);
